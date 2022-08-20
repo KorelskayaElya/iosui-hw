a854a9f085c6332814f5c84f6f1319492b054d15
Эля Корельская <korelskaya2000@mail.ru>
2022-08-20 22:56:56 +0300

    first commit

---
+++ Navigator/Navigator/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Navigator
+//
+//  Created by Эля Корельская on 20/08/22.
+//
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
