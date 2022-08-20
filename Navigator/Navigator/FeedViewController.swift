66101d7fefc57215a3c9bb463ff57b8070cb405c
Эля Корельская <korelskaya2000@mail.ru>
2022-08-20 23:36:33 +0300

    iosui-4

---
+++ Navigator/Navigator/FeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FeedViewController.swift
+//  Navigator
+//
+//  Created by Эля Корельская on 20/08/22.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    //лента пользователя
+    //var post = Post(title: "Мой пост")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemIndigo
+    }
+}
+
